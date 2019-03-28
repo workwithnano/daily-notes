9f9820f6da0b8356671b508473f87460544f6192
Nano Anderson <nano.anderson@gmail.com>
2019-03-28 13:18:22 -0700

    Allow reordering of today's todos

--- Create Today's TODOs.rb
+++ Create Today's TODOs.rb
@@ -1,4 +1,19 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
+
+ARGF.set_encoding(Encoding::UTF_8)
+
+def putsStringArray(stringArray)
+	stringArray.each_with_index do |line, index|
+		# remove the ending newline in the very last string
+		if index == stringArray.length - 1
+			line.gsub!(/(\r\n|\r|\n)$/, '')
+			print line
+		else
+			puts line
+		end
+	end
+end
 
 lines = Array.new(ARGF.readlines)
 linesCopy = Array.new(lines) # stdout the copy if we encounter an error
@@ -13,6 +28,15 @@ finishedTodo = false
 # Loop through all the selected lines
 lines.each_with_index do |line, idx|
 	
+	begin
+		line =~ /^\s*(\n\r)/
+	rescue StandardError => e
+		puts "Could not run line-end detection for the following line: "
+		puts line
+		puts e.message
+		puts e.backtrace.inspect
+	end
+	
 	if (idx == 0)
 		finishedTodos << line
 	elsif (idx == 1)
@@ -23,7 +47,7 @@ lines.each_with_index do |line, idx|
 		finishedTodos << ''
 		
 #################################################
-		
+	
 	elsif (line =~ /^\s*(\n\r)/)
 		# We are on an empty line
 		(finishedTodo ? finishedTodos : todaysTodos) << ''
@@ -40,11 +64,11 @@ lines.each_with_index do |line, idx|
 		# Check the start of the line for checkboxes or something else
 		if (line =~ /^- \[x\]/i)
 			finishedTodo = true
+			line.gsub!(/\*IN-PROGRESS\*\s+/, '') # remove IN-PROGRESS notice
 		else
 			finishedTodo = false
 		end
 		
-		unless finishedTodo
 			if (line =~ /\sROLLOVER/)
 				line.gsub!(/\sROLLOVER/, ' ROLLOVER+')
 			elsif (line =~ /\*IN-PROGRESS\*/) # Make sure in-progress status precedes rollover status
@@ -62,4 +86,8 @@ end
 # Put it in STDOUT
 todaysTodos.push('').concat(finishedTodos).each do |line|
 	puts line
+# If all Todos rolled over, say so.
+if !finishedTodos.last.strip.start_with?("-")
+	finishedTodos << "- All rolled over"
 end
+
